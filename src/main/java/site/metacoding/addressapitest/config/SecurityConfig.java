d8c9c57b7c0eaaabb2bf055b847565e34ba07c1c
Misterless <healwinds@gmail.com>
2022-05-18 16:22:37 +0900

    security설정확인 및 테스트 완료

---
+++ src/main/java/site/metacoding/addressapitest/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package site.metacoding.addressapitest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); 
+        http.authorizeRequests()
+        .antMatchers("/s/**").authenticated()
+        .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .loginPage("/loginForm")
+        .defaultSuccessUrl("/");
+       // super.configure(http);
+    }
+
+}
