1e5e794b7e7e19aad57ad27728a60be87bd5cb83
Misterless <healwinds@gmail.com>
2022-05-18 16:08:58 +0900

    Initial Commit

---
+++ src/main/java/site/metacoding/addressapitest/AddressApiTestApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.addressapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressApiTestApplication.class, args);
+	}
+
+}
